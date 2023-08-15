434cafd843f7ec8942571ec8248e278c5619aca7
TilakNaik <tilak.external@credable.in>
2023-08-15 17:01:21 +0530

    commit4

--- src/main/java/runner/RunnerClass.java
+++ src/main/java/runner/RunnerClass.java
@@ -3,7 +3,7 @@ package runner;
 import cucumber.api.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)//cucumber
 @CucumberOptions(features = "src/test/resources/featureFIles/FetchAnchorSummary.feature", glue = "step.definitions")
 
 public class RunnerClass {
